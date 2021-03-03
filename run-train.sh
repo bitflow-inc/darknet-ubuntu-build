c6add0928c06a8be45cb9078b2717a397b57f633
Sungjoon.Kim <methodtweak@naver.com>
2021-03-03 10:03:35 +0900

    initial commit

---
+++ run-train.sh
@@ -0,0 +1,1 @@
+./darknet detector train cfg/custom.data cfg/yolov4-custom.cfg cfg/yolov4.conv.137 | tee backup/train.log
---
+++ test-detect.sh
@@ -0,0 +1,1 @@
+./darknet detect cfg/yolov3_custom.cfg yolov3_custom.weights data/usd-5-dollars-paper-money-obverse.jpg
